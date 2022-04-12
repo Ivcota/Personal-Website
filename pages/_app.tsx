c797606c7e4cf9b018c1e08391b1e0555d1b1b1e
Ivcota <ivcotad@gmail.com>
2022-04-12 13:29:54 -0700

    custom _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import "@fontsource/inter";
+import "@fontsource/space-mono";
 import {
   ColorScheme,
   ColorSchemeProvider,
-  Global,
   MantineProvider,
 } from "@mantine/core";
-import "@fontsource/inter";
-import "@fontsource/space-mono";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 import { useState } from "react";
+import "../styles/globals.css";
 import Navbar from "./../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -17,49 +17,58 @@ function MyApp({ Component, pageProps }: AppProps) {
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
-    <ColorSchemeProvider
-      colorScheme={colorScheme}
-      toggleColorScheme={toggleColorScheme}
-    >
-      <MantineProvider
-        theme={{
-          fontFamily: "Space Mono",
-          fontFamilyMonospace: "Space Mono",
-          defaultRadius: "md",
-          headings: {
-            fontFamily: "Inter",
-          },
-          spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
-          breakpoints: {
-            xs: 500,
-            sm: 800,
-            md: 1000,
-            lg: 1200,
-            xl: 1400,
-          },
-          colors: {
-            dark: [
-              "#d5d7e0",
-              "#acaebf",
-              "#8c8fa3",
-              "#666980",
-              "#4d4f66",
-              "#34354a",
-              "#2b2c3d",
-              "#1d1e30",
-              "#0c0d21",
-              "#01010a",
-            ],
-          },
-
-          colorScheme: colorScheme,
-        }}
-        withGlobalStyles
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
       >
-        <Navbar />
-        <Component {...pageProps} />
-      </MantineProvider>
-    </ColorSchemeProvider>
+        <MantineProvider
+          theme={{
+            fontFamily: "Space Mono",
+            fontFamilyMonospace: "Space Mono",
+            defaultRadius: "md",
+            headings: {
+              fontFamily: "Inter",
+            },
+            spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
+            breakpoints: {
+              xs: 500,
+              sm: 800,
+              md: 1000,
+              lg: 1200,
+              xl: 1400,
+            },
+            colors: {
+              dark: [
+                "#d5d7e0",
+                "#acaebf",
+                "#8c8fa3",
+                "#666980",
+                "#4d4f66",
+                "#34354a",
+                "#2b2c3d",
+                "#1d1e30",
+                "#0c0d21",
+                "#01010a",
+              ],
+            },
+
+            colorScheme: colorScheme,
+          }}
+          withGlobalStyles
+        >
+          <Navbar />
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
   );
 }
 
